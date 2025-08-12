efbd27ad576f95f6ce9fde6ed472726d1575213a
YeryungCho <yeryung1128@gmail.com>
2025-08-12 22:22:57 +0900

    init: 프로젝트 기본 구조 설정

---
+++ localstep/src/main/java/com/codingmyeonga/localstep/LocalstepApplication.java
@@ -0,0 +1,13 @@
+package com.codingmyeonga.localstep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalstepApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LocalstepApplication.class, args);
+    }
+
+}
---
+++ localstep/src/test/java/com/codingmyeonga/localstep/LocalstepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingmyeonga.localstep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalstepApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
