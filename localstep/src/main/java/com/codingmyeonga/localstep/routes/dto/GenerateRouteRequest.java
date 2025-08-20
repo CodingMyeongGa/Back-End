e92a6cd97092c6fd40e21263d4dd2ff4ff965307
KimEunJi23 <yoonsdmswl4786@gmail.com>
2025-08-20 14:04:59 +0900

    0820routes 수정

--- localstep/src/main/java/com/codingmyeonga/localstep/routes/dto/GenerateRouteRequest.java
+++ localstep/src/main/java/com/codingmyeonga/localstep/routes/dto/GenerateRouteRequest.java
@@ -2,11 +2,18 @@ package com.codingmyeonga.localstep.routes.dto;
 
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
-//산책 루트를 생성할 때, 클라이언트가 보내는 데이터 구조
-@Getter @Setter @NoArgsConstructor @AllArgsConstructor
+
+//산책 루트 생성 시 클라이언트가 보내는 데이터 구조
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class GenerateRouteRequest {
-    @NotNull private Long user_id;
-    @NotNull private Double user_lat;
-    @NotNull private Double user_lng;
+    @NotNull
+    private Long user_id;
+    @NotNull
+    private Double user_lat;
+    @NotNull
+    private Double user_lng;
     private Integer goal_steps;
 }
